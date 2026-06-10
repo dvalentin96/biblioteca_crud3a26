0d1221a822b31c4e846ab619f95e23f4bb3192a9
dvalentin96 <wvalentind96@gmail.com>
2026-06-10 12:47:46 -0600

    Modelos de autor y carrera

--- models/usuario.py
+++ models/usuario.py
@@ -2,6 +2,7 @@ class usuario:
     def __init__(self, id_usuario, nombre, correo, carrera):
         self.id_usuario = id_usuario
         self.nombre = nombre
+        self.matricula = matricula
         self.correo = correo
         self.carrera = carrera
         self.activo = True  # por defecto, el usuario está activo
@@ -13,6 +14,7 @@ class usuario:
         self.activo = False
 
     def mostrar_info(self):
-        return f"Usuario ID: {self.id_usuario}, Nombre: {self.nombre}, Correo: {self.correo}, Carrera: {self.carrera}, Activo: {self.activo}"
+        return f"Usuario ID: {self.id_usuario}, Nombre: {self.nombre}, Matricula: {self.matricula}, Correo: {self.correo}, Carrera: {self.carrera}, Activo: {self.activo}"
+        
     
 
